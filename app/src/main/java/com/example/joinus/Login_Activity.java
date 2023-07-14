2e60f847a772c1482ab7123aff722f0cd68ae782
SeooyeonJ <130852344+SeooyeonJ@users.noreply.github.com>
2023-07-15 03:38:04 +0900

    Commit Message

--- app/src/main/java/com/example/joinus/Login_Activity.java
+++ app/src/main/java/com/example/joinus/Login_Activity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/joinus/MyPage_Activity.java
@@ -0,0 +1,15 @@
+package com.example.joinus;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPage_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
