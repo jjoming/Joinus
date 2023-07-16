59b7c1018fb64510702ca3676ef385073be8f9ac
SeooyeonJ <130852344+SeooyeonJ@users.noreply.github.com>
2023-07-16 15:55:22 +0900

    Commit Message

--- app/src/main/java/com/example/joinus/MyPage_Activity.java
+++ app/src/main/java/com/example/joinus/MyPage_Activity.java
@@ -1,15 +1,38 @@
 package com.example.joinus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPage_Activity extends AppCompatActivity {
 
+    ImageButton back;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        //TODO 툴바 뒤로가기 버튼
+        /*Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyPage_Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
