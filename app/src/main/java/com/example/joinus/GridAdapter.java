9a76224d3eae37fdcbd866e84911695a337e3b9a
minkyeongcho <jjoming129@naver.com>
2023-07-22 22:17:37 +0900

    Commit Message

--- app/src/main/java/com/example/joinus/GridAdapter.java
+++ app/src/main/java/com/example/joinus/GridAdapter.java
@@ -1,6 +1,14 @@
 package com.example.joinus;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +17,18 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class GridAdapter extends BaseAdapter {
 
     Context context;
     int[] imgIds; // 이미지 버튼에 들어갈 그림의 아이디
     int[] imgViewIds; // 이미지뷰에 들어갈 그림의 아이디
     LayoutInflater inflater;
+
+    File file;
+
+
 
     public GridAdapter(Context context, int[] imgIds, int[] imgViewIds) {
         this.context = context;
@@ -50,6 +64,9 @@ public class GridAdapter extends BaseAdapter {
         ImageButton imgBtn = convertView.findViewById(R.id.grid_btn);
         ImageView imgView = convertView.findViewById(R.id.imgView); // 이미지뷰 인플레이트
 
+        File sdcard = Environment.getExternalStorageDirectory();
+        file = new File(sdcard, "capture.jpg");
+
         imgBtn.setImageResource(imgIds[position]);
         //imgView.setImageResource(R.drawable.your_image_resource); // 이미지뷰에 원하는 이미지 설정
         imgView.setImageResource(imgViewIds[position]);
@@ -59,7 +76,7 @@ public class GridAdapter extends BaseAdapter {
             @Override
             public void onClick(View view) {
                 // todo : 카메라로 이동 후 확인 버튼 클릭시 체크표시로
-
+                capture();
                 handleImageButtonClick(position, imgView);
             }
         });
@@ -72,5 +89,22 @@ public class GridAdapter extends BaseAdapter {
         //
         imgView.setImageResource(R.drawable.icon_check_circle);
         // todo : 오늘의 목표 퍼센테이지 높이기
+    }
+
+    public void capture(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, 101);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 101  && resultCode == Activity.RESULT_OK){
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
