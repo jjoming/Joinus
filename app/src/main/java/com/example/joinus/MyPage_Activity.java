c0ab43ccb83381d8700ac17d68c435d3ca38f847
minkyeongcho <jjoming129@naver.com>
2023-07-23 02:10:15 +0900

    Commit Message

--- app/src/main/java/com/example/joinus/MyPage_Activity.java
+++ app/src/main/java/com/example/joinus/MyPage_Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
@@ -13,11 +14,17 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MyPage_Activity extends AppCompatActivity {
 
     ImageButton back;
+    ImageView[] imgViews;
+    int[] imgID = {R.id.img1, R.id.img2, R.id.img3, R.id.img4, R.id.img5, R.id.img6, R.id.img7, R.id.img8};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        for (int i = 0; i < imgID.length; i++) {
+            imgViews[i] = findViewById(imgID[i]);
+        }
 
         //TODO 툴바 뒤로가기 버튼
         /*Toolbar toolbar = findViewById(R.id.toolbar);
